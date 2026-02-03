5573ab9fc24c34a0eefc12e993db19c54f5993c9
Rajendra Prasad <rajendraprasad2049@gmail.com>
2026-02-03 14:16:54 +0530

    Inject AWS secret to validate SAST security gate

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
-# Intentionally insecure code for SAST gate testing
+print("Aegis pipeline alive")
+
+#  Intentionally injected secret for SAST gate test
 AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
 AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 
-
-print("Aegis pipeline alive")
