a2dc6128515527ac58f9b1233f0c301acfb3d7b8
Rajendra Prasad <rajendraprasad2049@gmail.com>
2026-02-03 18:52:55 +0530

    Prepare Flask app for DAST scanning

--- app/app.py
+++ app/app.py
@@ -1,4 +1,14 @@
-print("Aegis pipeline alive")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Aegis DevSecOps App Running"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
 
 
 
