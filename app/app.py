2fcbda746dcff8eefef7ea31ccf93ad9c5582eac
Rajendra Prasad <rajendraprasad2049@gmail.com>
2026-02-03 14:13:40 +0530

    Inject AWS credentials to validate SAST secrets enforcement

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
 # Intentionally insecure code for SAST gate testing
-HARDCODED_API_KEY = "sk_live_1234567890abcdef"
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
 
 print("Aegis pipeline alive")
